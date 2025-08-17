61a71fd6591619e3d03137bd1a0097ad25629d53
rehobbb <lzummx@gmail.com>
2025-08-18 07:57:16 +0800

    2025.08.17

---
+++ 01-chaoxian/1.py
@@ -0,0 +1,7 @@
+list = []
+i=0
+while i<10:
+    dict={'name':'zhangsan'+str(i),'age':str(i)}
+    list.append(dict)
+    i += 1
+print(list)
--- 01-chaoxian/xiaozhen.py
+++ 01-chaoxian/xiaozhen.py
@@ -1,34 +1,93 @@
-import re
-direct = input('please input the directory:')
-with open(direct + '\wdisp.out','r') as file:
-    lines = file.readlines()
-str1 = 'X 方向地震作用下的楼层最大位移'
-str2 = '=='
-results = []
-find_target = False
-i = 0
-while i < len(lines):
-    line = lines[i].strip()
-    if str1 in line:
-        find_target = True
-        i += 3
-        continue
-    if find_target:
-         if line and line[0].isdigit():
-            parts_1 = lines[i].split()
-            if int(parts_1[0]) < 100:
-                floor = int(parts_1[0])
-                parts_2 = re.split(r'\s{2,}',lines[i+1].strip())
-                disp = parts_2[3]
-                if '/ ' in disp:
-                    disp = disp.replace('/ ','/')
-                results.append([floor,disp])
+import re,csv,pandas as pd
+def findchunks(indicators,endflag,allfile):
+    chunks = []
+    for indicator in indicators.values():
+        i=0
+        findTarget = False
+        chunk = []
+        while i<len(allfile):
+            if indicator in allfile[i]:
+                findTarget = True
+                chunk.append(allfile[i])
                 i += 1
                 continue
-    if str2 in line:
-        break
-    i += 1
-for floor,disp in results:
-    print(floor,disp)
+            if findTarget:
+                if endflag in allfile[i]:
+                    break
+                else :
+                    chunk.append(allfile[i])
+                    i += 1
+                    continue
+            i += 1
+        chunks.append(chunk)
+    return chunks
 
+def is_contained(target,string_list):
+    return any(target in s for s in string_list)
+
+def get_disp_data(onechunk,index,data_a,indi_key,f_ratio):
+    i = 0
+    while i<len(onechunk):
+        parts1 = onechunk[i].split()
+        if parts1[0].isdigit() and int(parts1[0])<100:
+            floor = (int(parts1[0])) 
+            if is_contained(f_ratio,onechunk):
+                parts2 = onechunk[i+1].split()
+                value = max(float(parts1[index+2]),float(parts2[index]))
+            else:
+                 parts2 = re.split(r'\s{2,}',onechunk[i+1].strip())
+                 if 'w' in indi_key:
+                    value =parts2[index+1].replace('/ ','/')
+                 else:
+                    value =parts2[index].replace('/ ','/')
+            exist_dict = next((item for item in data_a if item['floor'] == floor), None)
+            if exist_dict:
+                exist_dict[indi_key] = value
+            else:
+                dict = {'floor':floor,indi_key:value}
+                data_a.append(dict)
+            i += 1
+            continue
+        i += 1
+
+direct1 = input('pleas input the directory:')
+direct = direct1 + '\\wdisp.out'
+d_index = 3
+f_ratio = '规定'
+with open(direct,'r') as file:
+    allfile = file.readlines()
+# 01. 找到对应字符串行范围
+indicators = {
+    'ex_disp':'X 方向地震作用下的楼层最大位移',
+    'ey_disp':'Y 方向地震作用下的楼层最大位移' ,
+    'wx_disp':'+X 方向风荷载作用下的楼层最大位移',
+    'wy_disp':'+Y 方向风荷载作用下的楼层最大位移',
+    'ex+_dratio':'X+ 偶然偏心规定水平力作用下的楼层最大位移',
+    'ex-_dratio':'X- 偶然偏心规定水平力作用下的楼层最大位移',
+    'ey+_dratio':'Y+ 偶然偏心规定水平力作用下的楼层最大位移',
+    'ey-_dratio':'Y- 偶然偏心规定水平力作用下的楼层最大位移',
+           }
+endflag = '=='
+datachunks = findchunks(indicators,endflag,allfile)
+filterdatachunks = [ [s for s in tmpchunk if s.strip()]
+    for tmpchunk in datachunks
+]
+# with open(direct1+'\\data.txt','w') as file2:
+#     for chunk in filterdatachunks:
+#         file2.writelines(chunk)
+#         file2.write('\n')
+# 02. 处理字符串
+data_a = []
+indi_keys = list(indicators.keys())
+indi_values = list(indicators.values())
+for onechunk,indi_key in zip(filterdatachunks ,indi_keys):
+    get_disp_data(onechunk,d_index,data_a,indi_key,f_ratio)
+with open(direct1+'\\data3.csv','w',newline='') as file3:
+    headers = data_a[0].keys()
+    writer = csv.DictWriter(file3,fieldnames=headers)
+    writer.writeheader()
+    writer.writerows(data_a)
+# 04. 存入到表格
+df = pd.DataFrame(data_a)
+df.to_excel(direct1+'\\data3.xlsx',index=False)
 
